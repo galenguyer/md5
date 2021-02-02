67400244abe0edbed28f832f6c7d158e9351eed6
Galen Guyer <galen@galenguyer.com>
2021-02-02 10:38:19 -0500

    Document test suite

--- c/test.c
+++ c/test.c
@@ -13,11 +13,13 @@ int run_test(char* name, long expected, long result) {
 
 int main(int argc, char** argv) {
     int passed = 0, failed = 0;
+    // compare core steps against RFC implementation
     run_test("step F", F(1, 2, 3), (((1) & (2)) | ((~1) & (3)))) ? passed++ : failed++;
     run_test("step G", G(1, 2, 3), (((1) & (3)) | ((2) & (~3)))) ? passed ++ : failed++;
     run_test("step H", H(1, 2, 3), ((1) ^ (2) ^ (3))) ? passed++ : failed++;
     run_test("step I", I(1, 2, 3), ((2) ^ ((1) | (~3)))) ? passed++ : failed++;
 
+    // compare ROTATE_LEFT against RFC implementation
     run_test("rotate_left", ROTATE_LEFT(24, 2), (((24) << (2)) | ((24) >> (32-(2))))) ? passed++ : failed++;
 
     printf("Tests Passed: %i\n", passed);
