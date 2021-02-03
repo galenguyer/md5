d4951c15c5c938dc5506d52f0c563583975b7be1
Galen Guyer <galen@galenguyer.com>
2021-02-03 08:59:24 -0500

    Add Colin Plumb implementation to tests

--- c/test.c
+++ c/test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "md5.c"
 
-int run_test(char* name, long expected, long result) {
+int run_test(char* name, long result, long expected) {
     if (expected == result) {
         printf("TEST PASSED: %s: expected %li, got %li\n", name, expected, result);
         return 1;
@@ -12,7 +12,7 @@ int run_test(char* name, long expected, long result) {
 }
 
 /* 
-The REFERENCE_ fuctions are licensed under the following license.
+The RFC_ fuctions are licensed under the following license.
 
 Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All
 rights reserved.
@@ -34,52 +34,95 @@ without express or implied warranty of any kind.
 
 These notices must be retained in any copies of any part of this
 documentation and/or software.
- */
+*/
 
-#define REFERENCE_F(x, y, z) (((x) & (y)) | ((~x) & (z)))
-#define REFERENCE_G(x, y, z) (((x) & (z)) | ((y) & (~z)))
-#define REFERENCE_H(x, y, z) ((x) ^ (y) ^ (z))
-#define REFERENCE_I(x, y, z) ((y) ^ ((x) | (~z)))
-#define REFERENCE_ROTATE_LEFT(x, n) (((x) << (n)) | ((x) >> (32-(n))))
-#define REFERENCE_FF(a, b, c, d, x, s, ac) ( \
-    (a) += REFERENCE_F ((b), (c), (d)) + (x) + (unsigned long int)(ac), \
-    (a) = REFERENCE_ROTATE_LEFT ((a), (s)), \
+#define RFC_F(x, y, z) (((x) & (y)) | ((~x) & (z)))
+#define RFC_G(x, y, z) (((x) & (z)) | ((y) & (~z)))
+#define RFC_H(x, y, z) ((x) ^ (y) ^ (z))
+#define RFC_I(x, y, z) ((y) ^ ((x) | (~z)))
+#define RFC_ROTATE_LEFT(x, n) (((x) << (n)) | ((x) >> (32-(n))))
+#define RFC_FF(a, b, c, d, x, s, ac) ( \
+    (a) += RFC_F ((b), (c), (d)) + (x) + (unsigned long int)(ac), \
+    (a) = RFC_ROTATE_LEFT ((a), (s)), \
     (a) += (b) \
 )
-#define REFERENCE_GG(a, b, c, d, x, s, ac) ( \
-    (a) += REFERENCE_G ((b), (c), (d)) + (x) + (unsigned long int)(ac), \
-    (a) = REFERENCE_ROTATE_LEFT ((a), (s)), \
+#define RFC_GG(a, b, c, d, x, s, ac) ( \
+    (a) += RFC_G ((b), (c), (d)) + (x) + (unsigned long int)(ac), \
+    (a) = RFC_ROTATE_LEFT ((a), (s)), \
     (a) += (b) \
 )
-#define REFERENCE_HH(a, b, c, d, x, s, ac) ( \
-    (a) += REFERENCE_H ((b), (c), (d)) + (x) + (unsigned long int)(ac), \
-    (a) = REFERENCE_ROTATE_LEFT ((a), (s)), \
+#define RFC_HH(a, b, c, d, x, s, ac) ( \
+    (a) += RFC_H ((b), (c), (d)) + (x) + (unsigned long int)(ac), \
+    (a) = RFC_ROTATE_LEFT ((a), (s)), \
     (a) += (b) \
 )
-#define REFERENCE_II(a, b, c, d, x, s, ac) ( \
-    (a) += REFERENCE_I ((b), (c), (d)) + (x) + (unsigned long int)(ac), \
-    (a) = REFERENCE_ROTATE_LEFT ((a), (s)), \
+#define RFC_II(a, b, c, d, x, s, ac) ( \
+    (a) += RFC_I ((b), (c), (d)) + (x) + (unsigned long int)(ac), \
+    (a) = RFC_ROTATE_LEFT ((a), (s)), \
     (a) += (b) \
 )
+
+/*
+ * The PLUMB_ fuctions are licensed under the following license.
+ *
+ * This code implements the MD5 message-digest algorithm.
+ * The algorithm is due to Ron Rivest.  This code was
+ * written by Colin Plumb in 1993, no copyright is claimed.
+ * This code is in the public domain; do with it what you wish.
+ *
+ * Equivalent code is available from RSA Data Security, Inc.
+ * This code has been tested against that, and is equivalent,
+ * except that you don't need to include two pages of legalese
+ * with every copy.
+ *
+ * To compute the message digest of a chunk of bytes, declare an
+ * MD5Context structure, pass it to MD5Init, call MD5Update as
+ * needed on buffers full of bytes, and then call MD5Final, which
+ * will fill a supplied 16-byte array with the digest.
+ */
+#define PLUMB_F1(x, y, z) (z ^ (x & (y ^ z)))
+#define PLUMB_F2(x, y, z) PLUMB_F1(z, x, y)
+#define PLUMB_F3(x, y, z) (x ^ y ^ z)
+#define PLUMB_F4(x, y, z) (y ^ (x | ~z))
+// NOTE: data = x + ac
+#define PLUMB_MD5STEP(f, w, x, y, z, data, s) \
+	( w += f(x, y, z) + data,  w = w<<s | w>>(32-s),  w += x )
+
 
 int main(int argc, char** argv) {
     int passed = 0, failed = 0;
+    puts("RFC Tests");
     // compare core steps against RFC implementation
-    run_test("F", F(1, 2, 3), REFERENCE_F(1, 2, 3)) ? passed++ : failed++;
-    run_test("G", G(1, 2, 3), REFERENCE_G(1, 2, 3)) ? passed ++ : failed++;
-    run_test("H", H(1, 2, 3), REFERENCE_H(1, 2, 3)) ? passed++ : failed++;
-    run_test("I", I(1, 2, 3), REFERENCE_I(1, 2, 3)) ? passed++ : failed++;
+    run_test("RFC_F", F(1, 2, 3), RFC_F(1, 2, 3)) ? passed++ : failed++;
+    run_test("RFC_G", G(1, 2, 3), RFC_G(1, 2, 3)) ? passed ++ : failed++;
+    run_test("RFC_H", H(1, 2, 3), RFC_H(1, 2, 3)) ? passed++ : failed++;
+    run_test("RFC_I", I(1, 2, 3), RFC_I(1, 2, 3)) ? passed++ : failed++;
 
     // compare ROTATE_LEFT against RFC implementation
-    run_test("ROTATE_LEFT", ROTATE_LEFT(24, 2), REFERENCE_ROTATE_LEFT(24, 2)) ? passed++ : failed++;
+    run_test("RFC_ROTATE_LEFT", ROTATE_LEFT(24, 2), RFC_ROTATE_LEFT(24, 2)) ? passed++ : failed++;
 
     // compare steps
     int a = 1;
     int ref_a = 1;
-    run_test("FF", STEP(F, a, 2, 3, 4, 5, 6, 7), REFERENCE_FF(ref_a, 2, 3, 4, 5, 6, 7)) ? passed++ : failed++;
-    run_test("GG", STEP(G, a, 2, 3, 4, 5, 6, 7), REFERENCE_GG(ref_a, 2, 3, 4, 5, 6, 7)) ? passed++ : failed++;
-    run_test("HH", STEP(H, a, 2, 3, 4, 5, 6, 7), REFERENCE_HH(ref_a, 2, 3, 4, 5, 6, 7)) ? passed++ : failed++;
-    run_test("II", STEP(I, a, 2, 3, 4, 5, 6, 7), REFERENCE_II(ref_a, 2, 3, 4, 5, 6, 7)) ? passed++ : failed++;
+    run_test("RFC_FF", STEP(F, a, 2, 3, 4, 5, 6, 7), RFC_FF(ref_a, 2, 3, 4, 5, 6, 7)) ? passed++ : failed++;
+    run_test("RFC_GG", STEP(G, a, 2, 3, 4, 5, 6, 7), RFC_GG(ref_a, 2, 3, 4, 5, 6, 7)) ? passed++ : failed++;
+    run_test("RFC_HH", STEP(H, a, 2, 3, 4, 5, 6, 7), RFC_HH(ref_a, 2, 3, 4, 5, 6, 7)) ? passed++ : failed++;
+    run_test("RFC_II", STEP(I, a, 2, 3, 4, 5, 6, 7), RFC_II(ref_a, 2, 3, 4, 5, 6, 7)) ? passed++ : failed++;
+
+    puts("Colin Plumb Tests");
+    // compare core steps against Colin Plumb implementation
+    run_test("PLUMB_F/F1", F(1, 2, 3), PLUMB_F1(1, 2, 3)) ? passed++ : failed++;
+    run_test("PLUMB_G/F2", G(1, 2, 3), PLUMB_F2(1, 2, 3)) ? passed ++ : failed++;
+    run_test("PLUMB_H/F3", H(1, 2, 3), PLUMB_F3(1, 2, 3)) ? passed++ : failed++;
+    run_test("PLUMB_I/F4", I(1, 2, 3), PLUMB_F4(1, 2, 3)) ? passed++ : failed++;
+
+    // Colin Plumb does not implement ROTATE_LEFT, it is implemented within the STEP function
+    a = 1;
+    int plumb_a  = 1;
+    run_test("PLUMB_FF", STEP(F, a, 2, 3, 4, 5, 6, 7), PLUMB_MD5STEP(PLUMB_F1, plumb_a, 2, 3, 4, 5 + 7, 6)) ? passed++ : failed++;
+    run_test("PLUMB_GG", STEP(G, a, 2, 3, 4, 5, 6, 7), PLUMB_MD5STEP(PLUMB_F2, plumb_a, 2, 3, 4, 5 + 7, 6)) ? passed++ : failed++;
+    run_test("PLUMB_HH", STEP(H, a, 2, 3, 4, 5, 6, 7), PLUMB_MD5STEP(PLUMB_F3, plumb_a, 2, 3, 4, 5 + 7, 6)) ? passed++ : failed++;
+    run_test("PLUMB_II", STEP(I, a, 2, 3, 4, 5, 6, 7), PLUMB_MD5STEP(PLUMB_F4, plumb_a, 2, 3, 4, 5 + 7, 6)) ? passed++ : failed++;
 
     printf("Tests Passed: %i\n", passed);
     printf("Tests Failed: %i\n", failed);
