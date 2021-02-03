9ad8bc828876dd9bc5bd694ed4e223824da508f3
Galen Guyer <galen@galenguyer.com>
2021-02-02 19:01:50 -0500

    Add license for reference code

--- c/test.c
+++ c/test.c
@@ -11,6 +11,31 @@ int run_test(char* name, long expected, long result) {
     }
 }
 
+/* 
+The REFERENCE_ fuctions are licensed under the following license.
+
+Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All
+rights reserved.
+
+License to copy and use this software is granted provided that it
+is identified as the "RSA Data Security, Inc. MD5 Message-Digest
+Algorithm" in all material mentioning or referencing this software
+or this function.
+
+License is also granted to make and use derivative works provided
+that such works are identified as "derived from the RSA Data
+Security, Inc. MD5 Message-Digest Algorithm" in all material
+mentioning or referencing the derived work.
+
+RSA Data Security, Inc. makes no representations concerning either
+the merchantability of this software or the suitability of this
+software for any particular purpose. It is provided "as is"
+without express or implied warranty of any kind.
+
+These notices must be retained in any copies of any part of this
+documentation and/or software.
+ */
+
 #define REFERENCE_F(x, y, z) (((x) & (y)) | ((~x) & (z)))
 #define REFERENCE_G(x, y, z) (((x) & (z)) | ((y) & (~z)))
 #define REFERENCE_H(x, y, z) ((x) ^ (y) ^ (z))
