522bf6fa30f9aaa8dd13e1ac7fbe75e6f36325e7
Galen Guyer <galen@galenguyer.com>
2021-02-04 22:56:53 -0500

    more tests and lib stuff

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 tests: md5.c tests.c
 	gcc -std=c99 -Wall -Werror=format-security -Werror=implicit-function-declaration tests.c md5.c -O2 -o tests
+
+lib: md5.c
+	gcc -std=c99 -Wall -Werror=format-security -Werror=implicit-function-declaration -shared -o libmd5.so md5.c
--- tests.c
+++ tests.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "md5.h"
 
@@ -12,14 +13,15 @@ int run_test(char* name, char* result, char* expected) {
     }
 }
 
-int main(int argc, char** argv) {
+int run_tests() {
     int passed = 0, failed = 0;
 
     run_test("md5(\"\")", md5(""), "d41d8cd98f00b204e9800998ecf8427e") ? passed++ : failed++;
     run_test("md5(\"a\")", md5("a"), "0cc175b9c0f1b6a831c399e269772661") ? passed++ : failed++;
     run_test("md5(\"abc\")", md5("abc"), "900150983cd24fb0d6963f7d28e17f72") ? passed++ : failed++;
     run_test("md5(\"message digest\")", md5("message digest"), "f96b697d7cb7938d525a2f31aaf161d0") ? passed++ : failed++;
-    run_test("md5(\"abcdefghijklmnopqrstuvwxyz\")", md5("abcdefghijklmnopqrstuvwxyz"), \
+    run_test("md5(\"abcdefghijklmnopqrstuvwxyz\")", \
+        md5("abcdefghijklmnopqrstuvwxyz"), \
         "c3fcd3d76192e4007dfb496cca67e13b") ? passed++ : failed++;
     run_test("md5(\"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\")", \
         md5("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"), \
@@ -31,4 +33,26 @@ int main(int argc, char** argv) {
     printf("Tests Passed: %i\n", passed);
     printf("Tests Failed: %i\n", failed);
     return failed;
+}
+
+int run_benchmarks(int test_count) {
+    printf("starting %d iterations of md5\n", test_count);
+    char* buffer = (char*) malloc(32);
+    for(int i = 0; i < test_count; i++) {
+        md5((char*)buffer);
+    }
+    return 0;
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        printf("md5 test options:\n\t-t | --test\n\t-b | --bench\n");
+        return 0;
+    }
+    if (argc == 2 && (strcmp(argv[1], "-t") == 0 || strcmp(argv[1], "--test") == 0)) {
+        return run_tests();
+    }
+    if (argc == 2 && (strcmp(argv[1], "-b") == 0 || strcmp(argv[1], "--bench") == 0)) {
+        return run_benchmarks(1000000);
+    }
 }
